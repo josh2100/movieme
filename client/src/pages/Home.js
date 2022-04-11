754767bace44f7c5a1b5be751b273dd8007dc859
joshgit <jschermann91@gmail.com>
2022-04-11 17:40:24 -0500

    added async state update

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -4,7 +4,7 @@ import { Redirect, useParams } from "react-router-dom";
 import { useQuery, useMutation } from "@apollo/client";
 import { SAVE_ARTICLE } from "../utils/mutations";
 import { QUERY_USER, QUERY_ME } from "../utils/queries";
-import { movieSearch } from "../utils/API";
+import { movieSearch, movieSearchByQuery } from "../utils/API";
 
 const Home = () => {
   // Check if logged in true or false with Auth.loggedIn();
@@ -15,13 +15,45 @@ const Home = () => {
   });
 
   const user = data?.me || data?.user || {};
-  const [articles, setArticles] = useState([]);
+  const [movies, setMovies] = useState([
+    {
+      title: "Star Wars: Episode IV - A New Hope",
+      Year: "1977",
+      imdbID: "tt0076759",
+      Type: "movie",
+      Poster:
+        "https://m.media-amazon.com/images/M/MV5BNzVlY2MwMjktM2E4OS00Y2Y3LWE3ZjctYzhkZGM3YzA1ZWM2XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg",
+    },
+    {
+      title: "Star Wars: Episode V - The Empire Strikes Back",
+      Year: "1980",
+      imdbID: "tt0080684",
+      Type: "movie",
+      Poster:
+        "https://m.media-amazon.com/images/M/MV5BYmU1NDRjNDgtMzhiMi00NjZmLTg5NGItZDNiZjU5NTU4OTE0XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg",
+    },
+    {
+      title: "Star Wars: Episode VI - Return of the Jedi",
+      Year: "1983",
+      imdbID: "tt0086190",
+      Type: "movie",
+      Poster:
+        "https://m.media-amazon.com/images/M/MV5BOWZlMjFiYzgtMTUzNC00Y2IzLTk1NTMtZmNhMTczNTk0ODk1XkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SX300.jpg",
+    },
+  ]);
   const [saveArticle] = useMutation(SAVE_ARTICLE);
 
-  const handleSearch = (genre) => {
-    console.log(genre);
-    movieSearch();
-  }
+  const handleSearch = async (genre) => {
+    try {
+      console.log(genre);
+      const data = await movieSearchByQuery(genre);
+      setMovies(data);
+      // setMovies(data);
+      console.log("movies:", movies);
+    } catch (error) {
+      throw error;
+    }
+  };
 
   const handleSaveArticle = async (element) => {
     saveArticle({
@@ -35,11 +67,30 @@ const Home = () => {
     });
   };
 
+  // const movieCards = () => {
+  //   if (movies) {
+  //     return <div>Movies present</div>;
+  //   }
+  // };
+
   return (
     <main className="row justify-content-evenly">
       <p>click a genre</p>
-      <button className="col-2"
-      onClick={()=>{handleSearch("random")}}>Random</button>
+      <button
+        className="col-2"
+        onClick={() => {
+          handleSearch("action");
+        }}
+      >
+        Random
+      </button>
+      <div className="">
+        {movies.map((movie) => (
+          <div key={movie.id} className="card">
+            movie<p>{movie.title}</p>
+          </div>
+        ))}
+      </div>
     </main>
   );
 };
--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -7,7 +7,23 @@ export const movieSearch = async () => {
 
     console.log(jsonMovie);
     return jsonMovie;
+  } catch (error) {
+    console.log(error);
+  }
+};
 
+export const movieSearchByQuery = async (genre) => {
+
+  try {
+    const movie = await fetch(
+      `https://api.themoviedb.org/3/search/movie?api_key=20de5a88f2545243e1111f80833e5b2e&query=${genre}`
+    );
+    const jsonMovie = await movie.json();
+
+    console.log("json movies", jsonMovie);
+    console.log("movie results from api", jsonMovie.results);
+    let returnedMovies = jsonMovie.results;
+    return returnedMovies;
   } catch (error) {
     console.log(error);
   }
