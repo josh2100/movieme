0722123e80d1b6e24a7299a016f04bd400c80721
joshgit <jschermann91@gmail.com>
2022-04-12 08:31:56 -0500

    removed hardcoding api test

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -16,30 +16,30 @@ const Home = () => {
 
   const user = data?.me || data?.user || {};
   const [movies, setMovies] = useState([
-    {
-      title: "Star Wars: Episode IV - A New Hope",
-      Year: "1977",
-      imdbID: "tt0076759",
-      Type: "movie",
-      Poster:
-        "https://m.media-amazon.com/images/M/MV5BNzVlY2MwMjktM2E4OS00Y2Y3LWE3ZjctYzhkZGM3YzA1ZWM2XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg",
-    },
-    {
-      title: "Star Wars: Episode V - The Empire Strikes Back",
-      Year: "1980",
-      imdbID: "tt0080684",
-      Type: "movie",
-      Poster:
-        "https://m.media-amazon.com/images/M/MV5BYmU1NDRjNDgtMzhiMi00NjZmLTg5NGItZDNiZjU5NTU4OTE0XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg",
-    },
-    {
-      title: "Star Wars: Episode VI - Return of the Jedi",
-      Year: "1983",
-      imdbID: "tt0086190",
-      Type: "movie",
-      Poster:
-        "https://m.media-amazon.com/images/M/MV5BOWZlMjFiYzgtMTUzNC00Y2IzLTk1NTMtZmNhMTczNTk0ODk1XkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SX300.jpg",
-    },
+    // {
+    //   title: "Star Wars: Episode IV - A New Hope",
+    //   Year: "1977",
+    //   imdbID: "tt0076759",
+    //   Type: "movie",
+    //   Poster:
+    //     "https://m.media-amazon.com/images/M/MV5BNzVlY2MwMjktM2E4OS00Y2Y3LWE3ZjctYzhkZGM3YzA1ZWM2XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg",
+    // },
+    // {
+    //   title: "Star Wars: Episode V - The Empire Strikes Back",
+    //   Year: "1980",
+    //   imdbID: "tt0080684",
+    //   Type: "movie",
+    //   Poster:
+    //     "https://m.media-amazon.com/images/M/MV5BYmU1NDRjNDgtMzhiMi00NjZmLTg5NGItZDNiZjU5NTU4OTE0XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg",
+    // },
+    // {
+    //   title: "Star Wars: Episode VI - Return of the Jedi",
+    //   Year: "1983",
+    //   imdbID: "tt0086190",
+    //   Type: "movie",
+    //   Poster:
+    //     "https://m.media-amazon.com/images/M/MV5BOWZlMjFiYzgtMTUzNC00Y2IzLTk1NTMtZmNhMTczNTk0ODk1XkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SX300.jpg",
+    // },
   ]);
   const [saveArticle] = useMutation(SAVE_ARTICLE);
 
@@ -66,12 +66,6 @@ const Home = () => {
       },
     });
   };
-
-  // const movieCards = () => {
-  //   if (movies) {
-  //     return <div>Movies present</div>;
-  //   }
-  // };
 
   return (
     <main className="row justify-content-evenly">
